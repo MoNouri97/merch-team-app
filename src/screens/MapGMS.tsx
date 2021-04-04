d503b9a71a590fe95a509b28a52691257a7c84ed
Mohamed Nouri <mohamed.nouri.1997@gmail.com>
2021-04-04 20:28:54 +0100

    Map page ui ✅

--- src/screens/MapGMS.tsx
+++ src/screens/MapGMS.tsx
@@ -1,6 +1,8 @@
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/core';
 import React from 'react';
 import { Alert, Dimensions } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 import AppText from '~/components/AppText';
 import Btn from '~/components/Btn';
 import { Subtitle } from '~/components/Forms/styles';
@@ -9,7 +11,7 @@ import styled from '~/config/styled-components';
 const { width, height } = Dimensions.get('screen');
 const MapGMS: React.FC = () => {
 	// code here ...
-	console.log('hello from MapGMS');
+	const navigation = useNavigation();
 	return (
 		<>
 			<MapView
@@ -24,10 +26,22 @@ const MapGMS: React.FC = () => {
 					width,
 					height,
 				}}
-			/>
+			>
+				<Marker
+					coordinate={{
+						latitude: 35.8146462228069,
+						longitude: 10.640393067151308,
+					}}
+					title="marker.title"
+					description="marker.description"
+				/>
+			</MapView>
+			<FloatingBtn onPress={() => navigation.goBack()}>
+				<Feather size={20} name="arrow-left" />
+			</FloatingBtn>
 			<Card>
 				<Col>
-					<Subtitle numberOfLines={2}>Aziza Swiss</Subtitle>
+					<Subtitle numberOfLines={2}>Aziza Ibn Khaldoun</Subtitle>
 					<AppText>15min</AppText>
 				</Col>
 				<Col>
@@ -41,8 +55,17 @@ const MapGMS: React.FC = () => {
 };
 const Col = styled.View`
 	flex: 1;
+	justify-content: center;
 `;
-
+const FloatingBtn = styled.TouchableOpacity`
+	position: absolute;
+	top: 50px;
+	left: 10px;
+	padding: 10px;
+	elevation: 5;
+	border-radius: ${({ theme }) => theme.borderRadius};
+	background: ${({ theme }) => theme.colors.white};
+`;
 const Card = styled.View`
 	position: absolute;
 	bottom: 10px;
@@ -54,5 +77,6 @@ const Card = styled.View`
 	border-radius: 25px;
 	flex-direction: row;
 	justify-content: space-between;
+	opacity: 0.9;
 `;
 export default MapGMS;
