2096f2cb60ccca36d55fc00c4ccc446117c798ba
Mohamed Nouri <mohamed.nouri.1997@gmail.com>
2021-04-07 13:45:35 +0100

    removed log from homestack

--- src/screens/HomeStack.tsx
+++ src/screens/HomeStack.tsx
@@ -8,23 +8,19 @@ import MapGMS from './MapGMS';
 import Report from './Report';
 
 const Stack = createStackNavigator();
-const HomeStack: React.FC = () => {
-	// code here ...
-	console.log('Hello From HomeStack');
-	return (
-		<Stack.Navigator headerMode="none">
-			<Stack.Screen name="Accueil" component={Home} />
-			<Stack.Screen name="MapGMS" component={MapGMS} />
-			<Stack.Screen
-				name="Report"
-				component={Report}
-				options={{
-					cardOverlayEnabled: true,
-					gestureEnabled: true,
-					...TransitionPresets.ModalSlideFromBottomIOS,
-				}}
-			/>
-		</Stack.Navigator>
-	);
-};
+const HomeStack: React.FC = () => (
+	<Stack.Navigator headerMode="none">
+		<Stack.Screen name="Accueil" component={Home} />
+		<Stack.Screen name="MapGMS" component={MapGMS} />
+		<Stack.Screen
+			name="Report"
+			component={Report}
+			options={{
+				cardOverlayEnabled: true,
+				gestureEnabled: true,
+				...TransitionPresets.ModalSlideFromBottomIOS,
+			}}
+		/>
+	</Stack.Navigator>
+);
 export default HomeStack;
