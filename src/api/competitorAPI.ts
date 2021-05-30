51cd4a2382fa7619540c42991637d813b68b4299
Mohamed Nouri <mohamed.nouri.1997@gmail.com>
2021-05-30 20:34:25 +0100

    competitor getone

--- src/api/competitorAPI.ts
+++ src/api/competitorAPI.ts
@@ -1,6 +1,7 @@
 /* eslint-disable arrow-body-style */
 import { useQuery } from 'react-query';
 import api from '~/config/api';
+import { QueryFn } from '~/types/ApiHelpers';
 import { Competitor } from '~/types/models/Competitor';
 
 const getCompetitors = async () => {
@@ -8,8 +9,18 @@ const getCompetitors = async () => {
 	return data;
 };
 
-const useGetCompetitors = () => {
-	return useQuery('get_competitors', getCompetitors);
+const useGetAllCompetitors = () => {
+	return useQuery('get_all_competitors', getCompetitors);
 };
 
-export default useGetCompetitors;
+const getCompetitor: QueryFn<Competitor, number> = async ({ queryKey }) => {
+	const [_, id] = queryKey;
+	const { data } = await api.get<Competitor>(`/competitor/${id}`);
+	return data;
+};
+
+const useGetCompetitor = (id: number) => {
+	return useQuery(['get_competitor', id], getCompetitor, { enabled: !!id });
+};
+
+export { useGetAllCompetitors, useGetCompetitor };
